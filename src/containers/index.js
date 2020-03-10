97277ec4020b3c4fe75addeede9ca7976eda2d7d
Adzkar Fauzie <adzkarfauzie02@gmail.com>
2020-03-11 02:19:25 +0700

    Add Gender Section

--- src/containers/index.js
+++ src/containers/index.js
@@ -5,3 +5,4 @@ export { default as Summary } from "./summary";
 export { default as LatestUpdate } from "./latestUpdate";
 export { default as News } from "./news";
 export { default as GrowthCases } from "./growthCases";
+export { default as Genders } from "./genders";
