f5f93bdbbfa704f8119446b68c1935c35ba6a831
andikaarif <andikapradanaarif@gmail.com>
2020-03-10 14:56:16 +0700

    change bg color

--- src/containers/description/style.js
+++ src/containers/description/style.js
@@ -4,7 +4,9 @@ export const DescriptionWrapper = styled.div`
   margin-top: 30px;
   background-color: #fff;
   border-radius: 6px;
-  padding: 15px 25px;
+  padding: 20px 25px;
+  box-shadow: 0px 0px 13px 0px rgba(236, 236, 241, 0.44);
+
   h3 {
     font-size: 14px;
     font-weight: 600;
